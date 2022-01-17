13875532e5224f7ec4495090d1150e79ffc84d35
João Vitor <joaovitormrn@gmail.com>
2022-01-17 14:05:59 -0300

    Implementado tela de pagamento

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -3,6 +3,7 @@ package br.com.alura.aluraviagens.ui.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
@@ -38,6 +39,9 @@ public class ResumoPacoteActivity extends AppCompatActivity {
         mostraDias(pacoteSaoPaulo);
         mostraPreco(pacoteSaoPaulo);
         mostraData(pacoteSaoPaulo);
+
+        Intent intent = new Intent(this, PagamentoActivity.class);
+        startActivity(intent);
     }
 
     private void mostraData(Pacote pacote) {
